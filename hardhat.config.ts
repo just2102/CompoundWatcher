c32f269ea3491fb14e426c5d4d0065df97fb3989
just2102 <thesixteensaltines@gmail.com>
2023-10-03 10:51:18 +0700

    add telegram notification

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,5 +1,7 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+import dotenv from "dotenv";
+dotenv.config();
 
 const config: HardhatUserConfig = {
   solidity: "0.8.18",
--- scripts/watch.ts
+++ scripts/watch.ts
@@ -1,8 +1,27 @@
 import { ethers } from "hardhat";
 import compoundAbi from "../abi/Compound.json";
+import axios from "axios";
 
-const wbtc = "0x2f2a2543b76a4166549f7aab2e75bef0aefc5b0f";
-const minThreshhold = ethers.parseUnits("0.00001", 8);
+const wbtc = "0x2f2a2543B76A4166549F7aaB2e75Bef0aefC5B0f";
+const minThreshhold = ethers.parseUnits("0.033", 8);
+
+const telegramToken = process.env.TELEGRAM_BOT_TOKEN;
+const chatId = process.env.TELEGRAM_CHAT_ID;
+
+async function sendTelegramMessage(message: string) {
+  console.log("sending telegram message");
+  const url = `https://api.telegram.org/bot${telegramToken}/sendMessage`;
+  const params = {
+    chat_id: chatId,
+    text: message,
+  };
+
+  try {
+    await axios.post(url, params);
+  } catch (error) {
+    console.error("Error sending Telegram message:", error);
+  }
+}
 
 async function main() {
   console.log("min threshhold", minThreshhold);
@@ -12,15 +31,13 @@ async function main() {
     ethers.provider
   );
   contract.on("WithdrawCollateral", (src, to, asset, amount) => {
-    amount = ethers.toBigInt(amount);
-    if (asset === wbtc && amount >= minThreshhold) {
+    if (asset.toString() === wbtc && amount >= minThreshhold) {
       console.log(`SOMEONE WITHDREW MORE THAN ${minThreshhold} WBTC!`);
+      sendTelegramMessage(`SOMEONE WITHDREW MORE THAN ${minThreshhold} WBTC!`);
     }
   });
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
