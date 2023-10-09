e47788061e242c8a7061ce7188963e49049cec08
just2102 <thesixteensaltines@gmail.com>
2023-10-09 11:11:52 +0700

    handle rpc error (too many requests)

--- scripts/watch.ts
+++ scripts/watch.ts
@@ -3,7 +3,7 @@ import compoundAbi from "../abi/Compound.json";
 import axios from "axios";
 
 const wbtc = "0x2f2a2543B76A4166549F7aaB2e75Bef0aefC5B0f";
-const minThreshhold = ethers.parseUnits("0.033", 8);
+const minThreshhold = ethers.parseUnits("0.02", 8);
 
 const telegramToken = process.env.TELEGRAM_BOT_TOKEN;
 const chatId = process.env.TELEGRAM_CHAT_ID;
@@ -24,21 +24,30 @@ async function sendTelegramMessage(message: string) {
 }
 
 async function main() {
-  console.log("min threshhold", minThreshhold);
-  const contract = new ethers.Contract(
-    "0x9c4ec768c28520B50860ea7a15bd7213a9fF58bf",
-    compoundAbi,
-    ethers.provider
-  );
-  contract.on("WithdrawCollateral", (src, to, asset, amount) => {
-    if (asset.toString() === wbtc && amount >= minThreshhold) {
-      console.log(`SOMEONE WITHDREW MORE THAN ${minThreshhold} WBTC!`);
-      sendTelegramMessage(`SOMEONE WITHDREW MORE THAN ${minThreshhold} WBTC!`);
-    }
-  });
+  try {
+    console.log("min threshhold", minThreshhold);
+    const contract = new ethers.Contract(
+      "0x9c4ec768c28520B50860ea7a15bd7213a9fF58bf",
+      compoundAbi,
+      ethers.provider
+    );
+    contract.on("WithdrawCollateral", (src, to, asset, amount) => {
+      if (asset.toString() === wbtc && amount >= minThreshhold) {
+        console.log(`SOMEONE WITHDREW MORE THAN ${minThreshhold} WBTC!`);
+        sendTelegramMessage(
+          `SOMEONE WITHDREW MORE THAN ${minThreshhold} WBTC!`
+        );
+      }
+    });
+  } catch (error) {
+    console.error(error);
+    process.exitCode = 1;
+
+    console.log("Waiting for 30 seconds before restarting...");
+    setTimeout(() => {
+      main();
+    }, 30000);
+  }
 }
 
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+main();
